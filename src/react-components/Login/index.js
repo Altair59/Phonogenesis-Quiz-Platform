b00ebed0c46dff632dc4e091c6409540b52afdc4
Snow <acceleratorsnow@gmail.com>
2020-02-28 03:22:31 -0500

    Simple Question Generator
    Simple Generator basic structure

--- src/react-components/Login/index.js
+++ src/react-components/Login/index.js
@@ -9,83 +9,82 @@ import "./styles.css";
 
 /* Component for the Home page */
 class Login extends React.Component {
-  state = {
-    redirect: null,
-    err: false
-  };
-  login = e => {
-    const filtered_user = this.props.users.filter(user => user.username === this.state.username)
-    if (filtered_user.length === 1) {
-      if (filtered_user[0].password === this.state.password) {
-        this.setState({
-          type: filtered_user[0].type,
-          name: filtered_user[0].name,
-          email: filtered_user[0].email,
-          username: filtered_user[0].username,
-          password: filtered_user[0].password
-        });
-        this.setState({redirect: "/"+filtered_user[0].type});
-      }
-      else {
-        this.setState({err: true})
-      }
-    } else if (filtered_user.length === 0) {
-      this.setState({err: true})
-    }
-  }
-
-  handleTextFieldChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  }
-
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to={{
-        pathname: this.state.redirect,
-        state: {
-          type: this.state.type,
-          name: this.state.name,
-          email: this.state.email,
-          username: this.state.username,
-          password: this.state.password
+    state = {
+        redirect: null,
+        err: false
+    };
+    login = e => {
+        const filtered_user = this.props.users.filter(user => user.username === this.state.username)
+        if (filtered_user.length === 1) {
+            if (filtered_user[0].password === this.state.password) {
+                this.setState({
+                    type: filtered_user[0].type,
+                    name: filtered_user[0].name,
+                    email: filtered_user[0].email,
+                    username: filtered_user[0].username,
+                    password: filtered_user[0].password
+                });
+                this.setState({redirect: "/" + filtered_user[0].type});
+            } else {
+                this.setState({err: true})
+            }
+        } else if (filtered_user.length === 0) {
+            this.setState({err: true})
         }
-      }} />
     }
 
-    return (
-      <div className="loginForm">
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <AccountCircle />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="username"
-              label="Username"
-              onChange={this.handleTextFieldChange}
-              error={this.state.err}
-              helperText={this.state.err ? "Incorrect username or password" : ''}
-            />
-          </Grid>
-        </Grid>
+    handleTextFieldChange = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
 
-        <div className="passwordForm">
-          <TextField
-            id="password"
-            label="Password"
-            onChange={this.handleTextFieldChange}
-            error={this.state.err}
-            helperText={this.state.err ? "Incorrect username or password" : ''}
-          />
-        </div>
-        <div className="loginButton">
-          <Button onClick={this.login}>Login</Button>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to={{
+                pathname: this.state.redirect,
+                state: {
+                    type: this.state.type,
+                    name: this.state.name,
+                    email: this.state.email,
+                    username: this.state.username,
+                    password: this.state.password
+                }
+            }}/>
+        }
+
+        return (
+            <div className="loginForm">
+                <Grid container spacing={1} alignItems="flex-end">
+                    <Grid item>
+                        <AccountCircle/>
+                    </Grid>
+                    <Grid item>
+                        <TextField
+                            id="username"
+                            label="Username"
+                            onChange={this.handleTextFieldChange}
+                            error={this.state.err}
+                            helperText={this.state.err ? "Incorrect username or password" : ''}
+                        />
+                    </Grid>
+                </Grid>
+
+                <div className="passwordForm">
+                    <TextField
+                        id="password"
+                        label="Password"
+                        onChange={this.handleTextFieldChange}
+                        error={this.state.err}
+                        helperText={this.state.err ? "Incorrect username or password" : ''}
+                    />
+                </div>
+                <div className="loginButton">
+                    <Button onClick={this.login}>Login</Button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Login;
--- src/react-components/SimpleGenerator/index.js
+++ src/react-components/SimpleGenerator/index.js
@@ -2,10 +2,108 @@ import React from "react";
 
 import "./styles.css";
 
+const QUESTION_SIZE_MIN = 15;
+const QUESTION_SIZE_MAX = 40;
+const RULE_SELECTION_MIN = 1;
+const RULE_SELECTION_MAX = 3;
+
 class SimpleGenerator extends React.Component {
-  render() {
-    return null;
-  }
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<form>
+				<RangeSelectionBlock rangeMax={RULE_SELECTION_MAX} rangeMin={RULE_SELECTION_MIN} allowEmpty={true}
+				                     default={''} title={"Rule NO. (range 1-3, blank for random)"}/>
+				<RangeSelectionBlock rangeMax={QUESTION_SIZE_MAX} rangeMin={QUESTION_SIZE_MIN} allowEmpty={true}
+				                     default={''} title={"Question Size (range 15-40)"}/>
+				<DropdownBlock options={["Random", "Alternating", "Neutralizing", "Mixed - Alternating & Neutralizing"]}
+				               default={"Random"} title={"Question Type"}/>
+				<DropdownBlock options={["Full Phonemes", "UR"]} default={"Full Phonemes"} title={"More Hints"}/>
+				<DropdownBlock options={["CADT", "CAND", "NCAD"]} default={"CADT"} title={"Question Type"}/>
+			</form>
+		);
+	};
+}
+
+// rangeMax, rangeMin, allowEmpty, default, title
+class RangeSelectionBlock extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {selection: props.default, warning: ''}
+	}
+
+	onBlur = (e) => {
+		const input = e.target.value;
+
+		if (!this.props.allowEmpty && input.length === 0) {     //  NUMBER OR BLANK ONLY
+			this.setState({warning: "can't be empty"});
+			return;
+		}
+
+		if (input.length > 0) {     //  RANGE CHECK IF IT'S NON-EMPTY
+			const selected = Number(input);
+
+			if (selected > this.props.rangeMax || selected < this.props.rangeMin) {
+				this.setState({warning: "out of range"})
+			}
+		}
+	};
+
+	onChange = (e) => {
+		const input = e.target.value;
+
+		if (input.match(/^\d*$/)) {      //  ONLY REGISTER INPUT IF IT'S A NUMBER
+			this.setState({selection: input, warning: ""})
+		}
+	};
+
+	render() {
+		const selection = this.state.selection;
+		const warning = this.state.warning;
+
+		return (
+			<div className={"formElement"}>
+				<legend className={"titleText"}>{this.props.title}</legend>
+				<div className={"inputBlock"}>
+					<input value={selection} onChange={this.onChange} onBlur={this.onBlur}/>
+					{warning === "" ? null : (<p className={"validateWarn"}>{warning}</p>)}
+				</div>
+			</div>
+		);
+	}
+}
+
+//  options, title, default
+class DropdownBlock extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {selection: props.default}
+	}
+
+	onChange = (e) => {
+		this.setState({selection: e.target.value})
+	};
+
+	render() {
+		const selection = this.state.selection;
+
+		return (
+			<div className={"formElement"}>
+				<legend className={"titleText"}>{this.props.title}</legend>
+				<select  className={"inputBlock"} onChange={this.onChange} value={selection}>
+					{this.props.options.map((option) =>
+						<option key={option}>{option}</option>
+					)}
+				</select>
+			</div>
+		);
+	}
 }
 
 export default SimpleGenerator;
