82749dd048b988969048c4acae4e91b61966733f
Joshua <joshua.han@mail.utoronto.ca>
2020-02-29 12:10:38 -0500

    Created a quiz tile react component that is added to the Statistics Page

---
+++ src/react-components/QuizTile.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Paper from '@material-ui/core/Paper'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+class QuizTile extends React.Component {
+  constructor(props) {
+		super(props);
+		this.state = {}
+	}
+
+  render() {
+    return(
+      <Paper elevation={3}>
+        <h4>Quiz #{this.props.quizId}</h4>
+        <p>Score: {this.props.score}</p>
+        <ExpansionPanel>
+         <ExpansionPanelSummary
+           expandIcon={<ExpandMoreIcon />}
+           aria-controls="panel1a-content"
+           id="panel1a-header"
+         >
+           More details
+         </ExpansionPanelSummary>
+         <ExpansionPanelDetails>
+           kajlfkaklsfj
+         </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </Paper>
+    )
+  }
+}
+
+export default QuizTile
