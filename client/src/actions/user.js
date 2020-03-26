4f9c30eb9a5b5c899a47ac6dd42c4a03e4a9e34a
Altair59 <39001328+Altair59@users.noreply.github.com>
2020-03-26 13:59:35 -0400

    trans from fecth to axios

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -34,31 +34,43 @@ export const login = (loginPage, loginProps) => {
 export const logout = (app) => {
 	const url = "http://127.0.0.1:9000/users/logout";
 
-	fetch(url, {credentials: 'same-origin'}).then(res => {
-		app.setState({
-			currentUser: null
-		});
-	}).catch(error => {
-		console.log(error);
-	})
+	// fetch(url, {credentials: 'same-origin'}).then(res => {
+	// 	app.setState({
+	// 		currentUser: null
+	// 	});
+	// }).catch(error => {
+	// 	console.log(error);
+	// })
 };
 
 export const getUsers = (page) => {
-	fetch("http://127.0.0.1:9000/users", {
-		method: 'GET',
-		credentials: 'same-origin'
-	}).then(res => {
+	// fetch("http://127.0.0.1:9000/users", {
+	// 	method: 'GET',
+	// 	credentials: 'same-origin'
+	// }).then(res => {
+	// 	res.json().then((result) => {
+	// 		page.setState({users: result.users});
+	// 	});
+	// });
+	axios.get("http://127.0.0.1:9000/users").then(res => {
 		res.json().then((result) => {
 			page.setState({users: result.users});
-		});
-	});
+		})
+	})
 };
 
 export const removeUser = (page, username) => {
-	const newURL = "http://127.0.0.1:9000/users/" + username;
-	fetch(newURL, {
-		method: 'DELETE',
-		credentials: 'same-origin'
+	//const newURL = "http://127.0.0.1:9000/users/" + username;
+	// fetch(newURL, {
+	// 	method: 'DELETE',
+	// 	credentials: 'same-origin'
+	// }).catch(error => {
+	// 	console.log(error)
+	// });
+	axios.delete("http://127.0.0.1:9000/users/", {
+		params: {
+			username: username
+		}
 	}).catch(error => {
 		console.log(error)
 	});
@@ -66,39 +78,61 @@ export const removeUser = (page, username) => {
 };
 
 export const addUser = (app) => {
-	const info = {
-		name: app.state.name,
-		type: app.state.type,
-		email: app.state.email,
-		username: app.state.username,
-		password: app.state.password,
-		groups: [],
-		quizzes: []
-	};
-
-	fetch("http://127.0.0.1:9000/users/", {
-		method: 'post',
-		credentials: 'same-origin',
-		body: JSON.stringify(info),
-		headers: {
-			Accept: "application/json, text/plain, */*",
-			"Content-Type": "application/json"
+	// const info = {
+	// 	name: app.state.name,
+	// 	type: app.state.type,
+	// 	email: app.state.email,
+	// 	username: app.state.username,
+	// 	password: app.state.password,
+	// 	groups: [],
+	// 	quizzes: []
+	// };
+	// fetch("http://127.0.0.1:9000/users/", {
+	// 	method: 'post',
+	// 	credentials: 'same-origin',
+	// 	body: JSON.stringify(info),
+	// 	headers: {
+	// 		Accept: "application/json, text/plain, */*",
+	// 		"Content-Type": "application/json"
+	// 	}
+	// }).then(result => {
+	// 	app.setState({currEdit: -1});
+	// }).catch(err => {
+	// 	console.log(err);
+	// });
+	axios.post("http://127.0.0.1:9000/users/", {
+		body: {
+			name: app.state.name,
+			type: app.state.type,
+			email: app.state.email,
+			username: app.state.username,
+			password: app.state.password,
+			groups: [],
+			quizzes: []
 		}
 	}).then(result => {
-		app.setState({currEdit: -1});
-	}).catch(err => {
-		console.log(err);
+		app.setState({currEdit: -1})
+	}).catch(error => {
+		console.log(error)
 	});
 	getUsers(app)
 };
 
 export const editUser = (page, username, info) => {
-	const newURL = "http://127.0.0.1:9000/users/" + JSON.stringify(username);
-	fetch(newURL, {
-		method: 'PATCH',
-		credentials: 'same-origin',
-		body: JSON.stringify(info),
-		headers: new Headers({'Content-Type': 'application/json'})
+	// const newURL = "http://127.0.0.1:9000/users/" + JSON.stringify(username);
+	// fetch(newURL, {
+	// 	method: 'PATCH',
+	// 	credentials: 'same-origin',
+	// 	body: JSON.stringify(info),
+	// 	headers: new Headers({'Content-Type': 'application/json'})
+	// }).catch(error => {
+	// 	console.log(error)
+	// });
+	axios({
+		method: 'patch',
+		url: "http://127.0.0.1:9000/users/",
+		params: username,
+		body: info
 	}).catch(error => {
 		console.log(error)
 	});
