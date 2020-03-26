c87b7f17a883bbd4cf69a9fe4e86599d5b18212e
Snow <acceleratorsnow@gmail.com>
2020-03-26 15:45:36 -0400

    Completed All Schema

---
+++ api/models/group.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const GroupSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 1,
+		unique: true,
+		trim: true
+	},
+	students: {
+		type: [String],
+		default: []
+	},
+	owner: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	}
+});
+
+const Group = mongoose.model('Group', GroupSchema);
+model.exports = {Group};
